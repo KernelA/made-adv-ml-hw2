1b5f5a249f898083ccb677b6101a2014356c6513
KernelA <17554646+KernelA@users.noreply.github.com>
2021-04-04 18:35:44 +0300

    Update test run

--- test_log_reg.py
+++ test_log_reg.py
@@ -1,4 +1,5 @@
 import torch
+from torch import optim
 from sklearn import datasets
 from sklearn.metrics import roc_auc_score
 import numpy as np
@@ -7,8 +8,8 @@ from rating_model import LogisticRegressionTorch, Trainer
 
 
 def train(x, y, model):
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    trainer = Trainer(model, optimizer, 400, torch.device("cpu"))
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    trainer = Trainer(model=model, em_step=0, optimizer=optimizer, num_iter=400, device=torch.device("cpu"))
 
     trainer.fit(torch.from_numpy(x), torch.from_numpy(y.astype(np.float32)).reshape(-1, 1))
 
